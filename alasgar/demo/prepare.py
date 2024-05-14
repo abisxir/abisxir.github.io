02e4fca77d656cb0472e9aaf25ee202d39e9783b
abisxir <abisxir@gmail.com>
2024-05-14 14:48:36 +0200

    Zinterval files added.

--- alasgar/demo/prepare.py
+++
@@ -1,6 +0,0 @@
-with open('build/index.html', 'r') as f:
-    html = f.read()
-with open('main.nim', 'r') as f:
-    nim = f.read()
-with open('build/index.html', 'w') as f:
-    f.write(html.replace('{{{SOURCE}}}', nim))
